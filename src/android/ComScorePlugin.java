bedb8ade09f5015e7c9c7d7cd38b682dbd5b3b23
jaanguita <jose.anguita@marfeel.com>
2018-10-21 19:39:02 +0200

    Android new library imported and can be used from the java files.  In this status compiles the ComScorePlugin.java importing and using tje new aar functionality.

--- src/android/ComScorePlugin.java
+++ src/android/ComScorePlugin.java
@@ -1,6 +1,8 @@
 package com.comscoreplugin;
 
-import com.comscore.analytics.comScore;
+import com.comscore.Analytics;
+import com.comscore.PublisherConfiguration;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import android.util.Log;
@@ -10,117 +12,123 @@ import org.json.JSONArray;
 
 public class ComScorePlugin extends CordovaPlugin {
 
-	public static final String TAG = "ComScorePlugin";
-	public static final String SETCUSTOMERDATA = "setCustomerData";
-	public static final String SETAPPNAME = "setAppName";
-	public static final String SETAPPCONTEXT = "setAppContext";
-	public static final String ONENTERFOREGROUND = "onEnterForeground";
-	public static final String ONEXITFOREGROUND = "onExitForeground";
-	public static final String AUTOUPDATEFOREGROUND = "autoUpdateForeground";
-	public static final String AUTOUPDATEBACKGROUND = "autoUpdateBackground";
-	public static final String START = "start";
+    public static final String TAG = "ComScorePlugin";
+    public static final String SETCUSTOMERDATA = "setCustomerData";
+    public static final String SETAPPNAME = "setAppName";
+    public static final String SETAPPCONTEXT = "setAppContext";
+    public static final String ONENTERFOREGROUND = "onEnterForeground";
+    public static final String ONEXITFOREGROUND = "onExitForeground";
+    public static final String AUTOUPDATEFOREGROUND = "autoUpdateForeground";
+    public static final String AUTOUPDATEBACKGROUND = "autoUpdateBackground";
+    public static final String START = "start";
 
 
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-		boolean result = false;
+        boolean result = false;
 
-		if (SETCUSTOMERDATA.equals(action)) {
-			this.setCustomerData(args.getString(0), args.getString(1), callbackContext);
-			result = true;
-		}
-		else if (SETAPPNAME.equals(action)) {
-			this.setAppName(args.getString(0), callbackContext);
-			result = true;
-		}
-		else if (SETAPPCONTEXT.equals(action)) {
-			this.setAppContext(callbackContext);
-			result = true;
-		}
-		else if (ONENTERFOREGROUND.equals(action)) {
-			this.onEnterForeground(callbackContext);
-			result = true;
-		}
-		else if (ONEXITFOREGROUND.equals(action)) {
-			this.onExitForeground(callbackContext);
-			result = true;
-		}
-		else if (AUTOUPDATEFOREGROUND.equals(action)) {
-			Integer interval = Integer.parseInt(args.getString(0));
-			this.autoUpdateForeground(interval, callbackContext);
-			result = true;
-		}
-		else if (AUTOUPDATEBACKGROUND.equals(action)) {
-			Integer interval = Integer.parseInt(args.getString(0));
-			this.autoUpdateBackground(interval, callbackContext);
-			result = true;
-		}
-		else if (START.equals(action)) {
-			this.start(callbackContext);
-			result = true;
-		}
+        if (SETCUSTOMERDATA.equals(action)) {
+            this.setCustomerData(args.getString(0), args.getString(1), callbackContext);
+            result = true;
+        }
+        else if (SETAPPNAME.equals(action)) {
+            this.setAppName(args.getString(0), callbackContext);
+            result = true;
+        }
+        else if (SETAPPCONTEXT.equals(action)) {
+            this.setAppContext(callbackContext);
+            result = true;
+        }
+        else if (ONENTERFOREGROUND.equals(action)) {
+            this.onEnterForeground(callbackContext);
+            result = true;
+        }
+        else if (ONEXITFOREGROUND.equals(action)) {
+            this.onExitForeground(callbackContext);
+            result = true;
+        }
+        else if (AUTOUPDATEFOREGROUND.equals(action)) {
+            Integer interval = Integer.parseInt(args.getString(0));
+            this.autoUpdateForeground(interval, callbackContext);
+            result = true;
+        }
+        else if (AUTOUPDATEBACKGROUND.equals(action)) {
+            Integer interval = Integer.parseInt(args.getString(0));
+            this.autoUpdateBackground(interval, callbackContext);
+            result = true;
+        }
+        else if (START.equals(action)) {
+            this.start(callbackContext);
+            result = true;
+        }
 
 
-		return result;
-	}
+        return result;
+    }
 
     @Override
     protected void pluginInitialize() {
-		comScore.setAppContext(this.webView.getContext());
-		Log.v(TAG, "pluginInitialize setAppContext");
+        //Analytics.setAppContext(this.webView.getContext());
+        Log.v(TAG, "pluginInitialize setAppContext");
     }
 
-	private void setCustomerData(String customerID, String customerKey, CallbackContext callbackContext) {
-		comScore.setCustomerC2(customerID);
-		Log.v(TAG, "setCustomerC2 " + customerID);
-		comScore.setPublisherSecret(customerKey);
-		Log.v(TAG, "setPublisherSecret " + customerKey);
-		callbackContext.success("ok");
-	}
+    private void setCustomerData(String customerID, String customerKey, CallbackContext callbackContext) {
+        PublisherConfiguration myPublisherConfig = new PublisherConfiguration.Builder()
+                .publisherId(customerID)
+                .publisherSecret(customerKey)
+                //.usagePropertiesAutoUpdateMode(usagePropertiesAutoUpdateMode.FOREGROUND_ONLY)
+                .build();
 
-	private void setAppName(String appName, CallbackContext callbackContext) {
-		comScore.setAppName(appName);
-		Log.v(TAG, "setAppName");
-		callbackContext.success("ok");
+        Log.v(TAG, "publisherId" + customerID);
+        Log.v(TAG, "publisherSecret " + customerKey);
+        Analytics.getConfiguration().addClient(myPublisherConfig);
+        Analytics.start(this.webView.getContext());
+        callbackContext.success("ok");
+    }
 
-	}
+    private void setAppName(String appName, CallbackContext callbackContext) {
+        //Analytics.setAppName(appName);
+        Log.v(TAG, "setAppName");
+        callbackContext.success("ok");
 
-	private void setAppContext(CallbackContext callbackContext) {
-		comScore.setAppContext(this.webView.getContext());
-		Log.v(TAG, "setAppContext");
-		callbackContext.success("ok");
-	}
+    }
 
-	private void onEnterForeground(CallbackContext callbackContext) {
-		comScore.onEnterForeground();
-		Log.v(TAG, "onEnterForeground");
-		callbackContext.success("ok");
-	}
+    private void setAppContext(CallbackContext callbackContext) {
+        //Analytics.setAppContext(this.webView.getContext());
+        Log.v(TAG, "setAppContext");
+        callbackContext.success("ok");
+    }
 
-	private void onExitForeground(CallbackContext callbackContext) {
-		comScore.onExitForeground();
-		Log.v(TAG, "onExitForeground");
-		callbackContext.success("ok");
-	}
+    private void onEnterForeground(CallbackContext callbackContext) {
+        //comScore.onEnterForeground();
+        //Analytics.notifyEnterForeground();
+        Log.v(TAG, "onEnterForeground");
+        callbackContext.success("ok");
+    }
 
-	private void autoUpdateForeground(Integer interval, CallbackContext callbackContext) {
-		comScore.enableAutoUpdate(interval, true);
-		Log.v(TAG, "autoUpdateForeground");
-		callbackContext.success("ok");
-	}
+    private void onExitForeground(CallbackContext callbackContext) {
+        //comScore.onExitForeground();
+        //Analytics.notifyExitForeground();
+        Log.v(TAG, "onExitForeground");
+        callbackContext.success("ok");
+    }
 
-	private void autoUpdateBackground(Integer interval, CallbackContext callbackContext) {
-		comScore.enableAutoUpdate(interval, false);
-		Log.v(TAG, "autoUpdateBackground");
-		callbackContext.success("ok");
-	}
+    private void autoUpdateForeground(Integer interval, CallbackContext callbackContext) {
+        //Analytics.enableAutoUpdate(interval, true);
+        Log.v(TAG, "autoUpdateForeground");
+        callbackContext.success("ok");
+    }
 
-	private void start(CallbackContext callbackContext) {
-		comScore.start();
-		Log.v(TAG, "start");
-		callbackContext.success("ok");
-	}
+    private void autoUpdateBackground(Integer interval, CallbackContext callbackContext) {
+        //Analytics.enableAutoUpdate(interval, false);
+        Log.v(TAG, "autoUpdateBackground");
+        callbackContext.success("ok");
+    }
 
-
+    private void start(CallbackContext callbackContext) {
+        Analytics.start(this.webView.getContext());
+        Log.v(TAG, "start");
+        callbackContext.success("ok");
+    }
 }
